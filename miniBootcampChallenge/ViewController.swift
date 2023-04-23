d77bb363ef066ad4b6fbf83252ffa32ff94fc323
Hario Budiharjo <hario.budiharjo@amarbank.co.id>
2023-04-24 02:22:59 +0700

    feat(download): Make download an image asynchronous function

--- miniBootcampChallenge/ViewController.swift
+++ miniBootcampChallenge/ViewController.swift
@@ -27,6 +27,23 @@ class ViewController: UICollectionViewController {
 
 
 // TODO: 1.- Implement a function that allows the app downloading the images without freezing the UI or causing it to work unexpected way
+extension ViewController {
+    /// that downloads an image asynchronously from a given URL using the URLSession API.
+    /// - Parameters:
+    ///   - url: a URL to download the image from.
+    ///   - completion: a completion handler with the downloaded image once it's available.
+    func downloadImage(from url: URL, completion: @escaping (UIImage?) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                completion(nil)
+                return
+            }
+            
+            let image = UIImage(data: data)
+            completion(image)
+        }.resume()
+    }
+}
 
 // TODO: 2.- Implement a function that allows to fill the collection view only when all photos have been downloaded, adding an animation for waiting the completion of the task.
 
@@ -41,10 +58,11 @@ extension ViewController {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.cellID, for: indexPath) as? ImageCell else { return UICollectionViewCell() }
         
         let url = urls[indexPath.row]
-        let data = try? Data(contentsOf: url)
-        let image = UIImage(data: data!)
-        cell.display(image)
-        
+        self.downloadImage(from: url) { image in
+            DispatchQueue.main.async {
+                cell.display(image)
+            }
+        }
         return cell
     }
 }
