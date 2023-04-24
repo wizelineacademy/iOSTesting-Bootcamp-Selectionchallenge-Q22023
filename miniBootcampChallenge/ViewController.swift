68995fbce28e0fe41d278fb1536703f37f9120da
Hario Budiharjo <hario.budiharjo@amarbank.co.id>
2023-04-24 20:26:09 +0700

    feat(download): Make multiple download images function and add animation for waiting the all of images downloaded

--- miniBootcampChallenge/ViewController.swift
+++ miniBootcampChallenge/ViewController.swift
@@ -16,10 +16,15 @@ class ViewController: UICollectionViewController {
     }
     
     private lazy var urls: [URL] = URLProvider.urls
+    private var images: [UIImage] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = Constants.title
+        self.downloadImages(from: urls) { images in
+            self.images = images
+            self.collectionView.reloadData()
+        }
     }
 
 
@@ -46,23 +51,48 @@ extension ViewController {
 }
 
 // TODO: 2.- Implement a function that allows to fill the collection view only when all photos have been downloaded, adding an animation for waiting the completion of the task.
-
+extension ViewController {
+    /// that downloads multiple images asynchronously from an array of URLs, and calls a completion handler with an array of downloaded.
+    /// - Parameters:
+    ///   - urls: a list URL to download the image from.
+    ///   - completion: a completion handler with the downloaded image once all of image it's available.
+    func downloadImages(from urls: [URL], completion: @escaping ([UIImage]) -> Void) {
+        var images = [UIImage]()
+        let group = DispatchGroup()
+        
+        let activityIndicator = UIActivityIndicatorView(style: .medium)
+        activityIndicator.center = view.center
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        
+        for url in urls {
+            group.enter()
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                defer { group.leave() }
+                guard let data = data, error == nil else { return }
+                if let image = UIImage(data: data) {
+                    images.append(image)
+                }
+            }.resume()
+        }
+        
+        group.notify(queue: .main) {
+            activityIndicator.stopAnimating()
+            activityIndicator.removeFromSuperview()
+            completion(images)
+        }
+    }
+}
 
 // MARK: - UICollectionView DataSource, Delegate
 extension ViewController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        urls.count
+        images.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.cellID, for: indexPath) as? ImageCell else { return UICollectionViewCell() }
-        
-        let url = urls[indexPath.row]
-        self.downloadImage(from: url) { image in
-            DispatchQueue.main.async {
-                cell.display(image)
-            }
-        }
+        cell.display(images[indexPath.row])
         return cell
     }
 }
